c5fa0cca89622fc4a174074701a7d3acf212dd42
cheng.zhang <mr.cheng94@foxmail.com>
2019-10-25 19:10:10 +0800

    Initial commit

---
+++ power-sharding-jdbc/src/main/java/com/sailfish/powershardingjdbc/PowerShardingJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.sailfish.powershardingjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerShardingJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PowerShardingJdbcApplication.class, args);
+    }
+
+}
---
+++ power-sharding-jdbc/src/test/java/com/sailfish/powershardingjdbc/PowerShardingJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sailfish.powershardingjdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerShardingJdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
